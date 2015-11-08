95b9df4b4b0dda93cbcdb5facf8aa8255ebef8fd
xgenadam <xgenadam@gmail.com>
2015-11-08 23:20:59 +0000

    renamed main.py to cbm.py, shorthand for cerebelum. started creation on testing suite

---
+++ test/test.py
@@ -0,0 +1,25 @@
+#testing for neural network
+import unittest
+import ../Aegir.py
+
+
+#create and gate
+
+
+#create or gate
+
+#create xor gate
+
+
+
+class neural_network_test():
+
+    #first test if and gate can be made
+    def test_and():
+
+    #then test or
+    def test_or():
+
+
+    #then test for xor
+    def test_xor():
